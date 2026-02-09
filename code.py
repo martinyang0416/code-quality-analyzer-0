f122c6f8703ff72ee0497cd60f32da5067fc54ac
Analyzer <analyzer@test.com>
2026-02-09 13:24:25 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,14 @@
-n, *rest = map(int, open(0).read().split())
-a = rest[:n]
-mod = n - 1
-remainder_map = {}
-for num in a:
-    r = num % mod
-    if r in remainder_map:
-        print(remainder_map[r], num)
-        break
-    remainder_map[r] = num
+n = int(input())
+if n < 3:
+    print(0)
+else:
+    degrees = [0] * (n + 1)
+    for _ in range(n - 1):
+        u, v = map(int, input().split())
+        degrees[u] += 1
+        degrees[v] += 1
+    total = n * (n - 1) * (n - 2) // 6
+    sum_edges = (n - 1) * (n - 2)
+    sum_pairs = sum(d * (d - 1) // 2 for d in degrees)
+    result = total - sum_edges + sum_pairs
+    print(max(0, result))
