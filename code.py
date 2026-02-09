f5a4a065914d0c718cc5a7adc72ffb0d3a42a08e
Analyzer <analyzer@test.com>
2026-02-09 10:28:38 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,7 @@
-def is_valid_encoded_message(S, E):
-    reversed_S = S[::-1]
-    if len(E) != 2 * len(S):
-        return False
-    for k in range(len(S) + 1):
-        if E == S[:k] + reversed_S + S[k:]:
-            return True
-    return False
-
 T = int(input())
 for _ in range(T):
-    S, E = input().split()
-    if is_valid_encoded_message(S, E):
-        print("Valid")
-    else:
-        print("Invalid")
+    N = int(input())
+    for i in range(1, N + 1):
+        numbers = list(range(1, i + 1))
+        line = ' '.join(map(str, numbers))
+        print(line)
