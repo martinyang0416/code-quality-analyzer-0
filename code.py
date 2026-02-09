d2de7a8dc989645a5b4bccae0ffa839a6322561a
Analyzer <analyzer@test.com>
2026-02-09 15:12:33 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,20 @@
-T = int(input())
-for _ in range(T):
-    prices = list(map(int, input().split()))
-    s = input().strip()
-    present = set(s)
-    total = 0
-    for i in range(26):
-        if chr(ord('a') + i) not in present:
-            total += prices[i]
-    print(total)
+# Precompute the last digits of the first 60 Fibonacci numbers
+fib_last_digits = [0] * 60
+fib_last_digits[0] = 0
+if len(fib_last_digits) > 1:
+    fib_last_digits[1] = 1
+    prev, curr = 0, 1
+    for i in range(2, 60):
+        next_val = (prev + curr) % 10
+        fib_last_digits[i] = next_val
+        prev, curr = curr, next_val
+
+import sys
+
+def main():
+    input = sys.stdin.read
+    data = input().split()
+    T = int(data[0])
+    for i in range(1, T+1):
+        N = int(data[i])
+        if N == 0
