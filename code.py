e97d160bf31f2edeb62bcc34757ed0e7d065cf12
Analyzer <analyzer@test.com>
2026-02-09 14:48:25 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,10 @@
-from collections import defaultdict
-
-def subarraySum(nums, k):
-    count = 0
-    prefix_sum = defaultdict(int)
-    prefix_sum[0] = 1
-    current_sum = 0
+def increasingTriplet(nums):
+    first = second = float('inf')
     for num in nums:
-        current_sum += num
-        count += prefix_sum.get(current_sum - k, 0)
-        prefix_sum[current_sum] += 1
-    return count
+        if num <= first:
+            first = num
+        elif num <= second:
+            second = num
+        else:
+            return True
+    return False
