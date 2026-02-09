22a8df1fef3bb4b269b3a9381fe26087cb86e43c
Analyzer <analyzer@test.com>
2026-02-09 10:34:32 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,4 @@
-import sys
-
-def main():
-    n, m = map(int, sys.stdin.readline().split())
-    # Read m edges (not used in the solution)
-    for _ in range(m):
-        a, b = map(int, sys.stdin.readline().split())
-    t = list(map(int, sys.stdin.readline().split()))
-    
-    # Check if all topics are unique and cover 1..n
-    topic_set = set()
-    for num in t:
-        if num < 1 or num > n:
-            print(-1)
-            return
-        if num in topic_set:
-            print(-1)
-            return
-        top
+n = int(input())
+a = list(map(int, input().split()))
+unique_a = set(a)
+print(sum(unique_a))
