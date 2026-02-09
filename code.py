afc53f7eebebbe2fbea76f08463671e614b3f8f7
Analyzer <analyzer@test.com>
2026-02-09 14:11:02 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,6 @@
-import sys
-from sys import stdin
-from sys import setrecursionlimit
-
-def main():
-    sys.setrecursionlimit(1 << 25)
-    n, m, k = map(int, stdin.readline().split())
-    adj = [[] for _ in range(n+1)]
-    for _ in range(m):
-        a, b = map(int, stdin.readline().split())
-        adj[a].append(b)
-        adj[b].append(a)
-    
-    visited = [False] * (n + 1)
-    path_stack = []
-    pos = [-1] * (n + 1)
-    
-    for start in range(1, n+1):
-        if not visited[start]:
-            stack = [(start,
+n, m = map(int, input().split())
+total = 0
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    total += a + b + c
+print(total % 97)
