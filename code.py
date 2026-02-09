e8334768b87e117e70c4ee6c3985bf0e560d8df2
Analyzer <analyzer@test.com>
2026-02-09 13:24:00 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,10 @@
-import sys
-
-def find_short_phrase_start(words):
-    n = len(words)
-    for start in range(n):
-        # Check group 1: sum 5
-        g1_sum = 0
-        g1_end = start
-        while g1_end < n:
-            g1_sum += len(words[g1_end])
-            if g1_sum == 5:
-                break
-            elif g1_sum > 5:
-                g1_sum = -1
-                break
-            g1_end += 1
-        if g1_sum != 5:
-            continue
-        
-        # Check group 2: sum 7
-        g2_start = g1_end + 
+n, *rest = map(int, open(0).read().split())
+a = rest[:n]
+mod = n - 1
+remainder_map = {}
+for num in a:
+    r = num % mod
+    if r in remainder_map:
+        print(remainder_map[r], num)
+        break
+    remainder_map[r] = num
