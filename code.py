b4733af1855ba15c74aedcf3130d088199e7e39d
Analyzer <analyzer@test.com>
2026-02-09 10:37:04 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,15 @@
-n = int(input())
-distances = list(map(int, input().split()))
-total = sum(distances)
-target = total / 2
-
-current_sum = 0
-for i in range(n):
-    current_sum += distances[i]
-    if current_sum >= target:
-        print(i + 1)
-        break
+def main():
+    import sys
+    input = sys.stdin.read().split()
+    M = int(input[0])
+    arr = list(map(int, input[1:M+1]))
+    arr.sort()
+    
+    # Case 1: R is max, S is min. P and Q are second and third max.
+    case1 = (arr[-2] + arr[-3]) * (arr[-1] - arr[0])
+    
+    # Case 2: R is second max, S is min. P and Q are max and third max.
+    case2 = (arr[-1] + arr[-3]) * (arr[-2] - arr[0])
+    
+    # Case 3: R is max, S is min. P and Q are max and second max.
+    case3 = (arr[-1] + arr[-2]) *
