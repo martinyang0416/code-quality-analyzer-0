df50819640eebcc5adcf6a9125dc317b385d7c06
Analyzer <analyzer@test.com>
2026-02-09 14:35:02 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,9 @@
-import heapq
-
-def minCost(grid):
-    m = len(grid)
-    n = len(grid[0])
-    if m == 1 and n == 1:
-        return 0
-    dirs = {1: (0, 1), 2: (0, -1), 3: (1, 0), 4: (-1, 0)}
-    heap = []
-    heapq.heappush(heap, (0, 0, 0))
-    dist = [[float('inf')] * n for _ in range(m)]
-    dist[0][0] = 0
-    target = (m-1, n-1)
-    
-    while heap:
-        cost, i, j = heapq.heappop(heap)
-        if (i, j) == target:
-            return cost
-        if cost > dist[i][j]:
-            continue
-        original_d
+def count_double_concatenations(text):
+    s = set()
+    n = len(text)
+    for L in range(2, n + 1, 2):
+        m = L // 2
+        for i in range(n - L + 1):
+            if text[i:i+m] == text[i+m:i+L]:
+                s.add(text[i:i+L])
+    return len(s)
