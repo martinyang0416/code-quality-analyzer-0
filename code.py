cd3e4b14096f5c924dd4fb0a8d932a4d85919deb
Analyzer <analyzer@test.com>
2026-02-09 15:18:15 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,2 @@
-import sys
-from collections import defaultdict
-
-def main():
-    input = sys.stdin.read().split()
-    ptr = 0
-    T = int(input[ptr])
-    ptr += 1
-    for _ in range(T):
-        N, K = int(input[ptr]), int(input[ptr+1])
-        ptr +=2
-        events = defaultdict(list)
-        for _ in range(N):
-            s = int(input[ptr])
-            e = int(input[ptr+1])
-            p = int(input[ptr+2])
-            ptr +=3
-            events[p].append((s, e))
-        total = 0
-        for room in events:
+n = int(input())
+print(n + 1 if n % 4 == 0 else n - 1)
