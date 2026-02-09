913c0b3ea8affb361010c17fdc57832acd7d9461
Analyzer <analyzer@test.com>
2026-02-09 10:36:15 -0800

    Update

--- code.py
+++ code.py
@@ -1,6 +1,28 @@
 n = int(input())
 
-if n == 1:
-    print("{0:.5f}".format(1.0))
-else:
-    print("{0:.5f}".format(0.5))
+even = []
+odd = []
+
+for num in range(1, n * n + 1):
+    if num % 2 == 0:
+        even.append(num)
+    else:
+        odd.append(num)
+
+matrix = []
+e_ptr = 0
+o_ptr = 0
+
+for i in range(n):
+    if i % 2 == 0:
+        row = []
+        for j in range(n // 2):
+            row.append(even[e_ptr])
+            e_ptr += 1
+        for j in range(n // 2):
+            row.append(even[e_ptr])
+            e_ptr += 1
+        matrix.append(row)
+    else:
+        row = []
+        for j in range(n 
