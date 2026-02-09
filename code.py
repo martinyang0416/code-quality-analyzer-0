dfdc1103a9b81eecdf1e25502639c1c571f2547e
Analyzer <analyzer@test.com>
2026-02-09 11:31:06 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,22 @@
 import sys
-from itertools import combinations
+
+class TrieNode:
+    __slots__ = ['children']
+    def __init__(self):
+        self.children = [None, None]
 
 def main():
     C, N = map(int, sys.stdin.readline().split())
     masks = []
-    mask_set = set()
-
     for _ in range(N):
         s = sys.stdin.readline().strip()
         mask = 0
         for c in s:
             mask = (mask << 1) | (1 if c == 'H' else 0)
         masks.append(mask)
-        mask_set.add(mask)
-
-    max_distances = []
-
-    for a in masks:
-        complement = ((1 << C) - 1) ^ a
-        if complement in mask_set:
-            max_distanc
+    
+    # Build the trie
+    root = TrieNode()
+    for mask in masks:
+        node = root
+        for i in reversed(ran
