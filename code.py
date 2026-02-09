d05e94f048887ff32bcca1218486171a7ce36931
Analyzer <analyzer@test.com>
2026-02-09 14:46:50 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,23 @@
-def rankTeams(votes):
-    if not votes:
-        return ""
-    n = len(votes[0])
-    teams = list(votes[0])
-    count = {team: [0] * n for team in teams}
-    for vote in votes:
-        for pos, team in enumerate(vote):
-            count[team][pos] += 1
-    sorted_teams = sorted(teams, key=lambda x: (tuple(-count[x][i] for i in range(n)), x))
-    return ''.join(sorted_teams)
+MOD = 10**9 + 7
+
+def count_ways(s):
+    n = len(s)
+    prefix = [0] * (n + 1)
+    for i in range(n):
+        prefix[i+1] = prefix[i] + (1 if s[i] == '1' else 0)
+    
+    total_ones = prefix[-1]
+    if total_ones % 3 != 0:
+        return 0
+    
+    if total_ones == 0:
+        return ((n-1) * (n-2) // 2) % MOD
+    
+    target = total_ones // 3
+    first_candidates = []
+    for i in range(n):
+        if prefix[i+1] == target:
+            first_candidates.append(i)
+    
+    second_candidates = []
+  
