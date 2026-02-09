6e83c9d7a65c454cc5ee0248d742dd8589da5d0a
Analyzer <analyzer@test.com>
2026-02-09 10:33:39 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,27 @@
 import sys
-from collections import defaultdict
+sys.setrecursionlimit(1 << 25)
 
 def main():
-    E_M = sys.stdin.readline().split()
-    while len(E_M) < 2:
-        line = sys.stdin.readline()
-        if not line:
-            break
-        E_M += line.split()
-    E = int(E_M[0])
-    M = int(E_M[1])
-
-    emoticons = []
-    for _ in range(E):
-        line = sys.stdin.readline().strip()
-        while not line:
-            line = sys.stdin.readline().strip()
-        emoticons.append(line)
-
-    # Build prefix map
-    prefix_map = def
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    n = int(data[0])
+    m = int(data[1])
+    
+    if m == 0:
+        print(0)
+        return
+    
+    parent = list(range(n + 1))
+    
+    def find(u):
+        while parent[u] != u:
+            parent[u] = parent[parent[u]]
+            u = parent[u]
+        return u
+    
+    def union(u, v):
+        u_root = find(u)
+        v_root = find(v)
+        if u_root == v_root:
+   
