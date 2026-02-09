027819d5ed6ec0ec8613654a45446e9a59360198
Analyzer <analyzer@test.com>
2026-02-09 15:04:14 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,5 @@
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    idx = 0
-    T = int(input[idx])
-    idx += 1
-    for _ in range(T):
-        N, K = map(int, input[idx:idx+2])
-        idx +=2
-        A = list(map(int, input[idx:idx+N]))
-        idx += N
-        
-        # Compute inv_A: inversions in original array
-        inv_A = 0
-        for i in range(N):
-            for j in range(i+1, N):
-                if A[i] > A[j]:
-                    inv_A += 1
-        
-        # Compute cross_pair
+n = int(input())
+result = 0
+for x in range(1, n):
+    result += (n - 1) // x
+print(result)
