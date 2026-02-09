31517c0ad32e71452f98dc0ab167b00d30d1fe79
Analyzer <analyzer@test.com>
2026-02-09 10:18:49 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,15 @@
-MOD = 10**9 + 7
+s = input().strip()
+vowels = {'a', 'e', 'i', 'o', 'u'}
+v_count = 0
+c_count = 0
 
-n, k = map(int, input().split())
-m = n + k - 1
-
-fact = [1] * (m + 1)
-for i in range(1, m + 1):
-    fact[i] = fact[i-1] * i % MOD
-
-inv_fact = [1] * (m + 1)
-inv_fact[m] = pow(fact[m], MOD - 2, MOD)
-for i in range(m - 1, -1, -1):
-    inv_fact[i] = inv_fact[i + 1] * (i + 1) % MOD
-
-if k == 0:
-    print(1)
-else:
-    if m < k:
-        print(0)
+for char in s:
+    if char in vowels:
+        v_count += 1
     else:
-        res = fact[m] * inv_fact[k] % MOD
-        res = res * inv_fact[m - k] % MOD
-        print(res)
+        c_count += 1
+
+if v_count == c_count:
+    print("Balanced")
+else:
+    print("Unbalanced")
