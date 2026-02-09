cd7024079f7adadd01cd64fae615031db086e03d
Analyzer <analyzer@test.com>
2026-02-09 10:06:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,3 @@
-n, q = map(int, input().split())
-
-lower = [1] * (n + 1)  # 1-based indexing
-upper = [n] * (n + 1)
-
-for _ in range(q):
-    t, l, r, v = map(int, input().split())
-    if t == 1:
-        # At least v, update lower bound
-        for i in range(l, r + 1):
-            if lower[i] < v:
-                lower[i] = v
-    else:
-        # At most v, update upper bound
-        for i in range(l, r + 1):
-            if upper[i] > v:
-                upper[i] = v
-
-# Check for feasibility
-possible = True
-for i in
+n = int(input())
+student_ids = list(map(int, input().split()))
+print(len(set(student_ids)))
