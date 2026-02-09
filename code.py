2cc92bb4adee9c81a921886bb8d0bc9f3a2e10a7
Analyzer <analyzer@test.com>
2026-02-09 14:17:01 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,10 @@
-def max_jumps(arr, d):
-    n = len(arr)
-    dp = [1] * n
-    indices = sorted([(arr[i], i) for i in range(n)], key=lambda x: (x[0], x[1]))
-    
-    for a, i in indices:
-        possible_js = []
-        
-        # Check left direction
-        current_max = 0
-        for x in range(1, min(d, i) + 1):
-            j = i - x
-            if j < 0:
-                break
-            if arr[j] >= arr[i]:
-                break
-            if x > 1:
-                new_element = arr[j + 1]
-                
+def isValidSerialization(preorder):
+    nodes = preorder.split(',')
+    slots = 1
+    for node in nodes:
+        if slots <= 0:
+            return False
+        slots -= 1
+        if node != '#':
+            slots += 2
+    return slots == 0
