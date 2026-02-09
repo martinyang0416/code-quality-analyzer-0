346edac3f0877de8408713413f12dbc8dc6cb921
Analyzer <analyzer@test.com>
2026-02-09 10:36:38 -0800

    Update

--- code.py
+++ code.py
@@ -1,28 +1,11 @@
 n = int(input())
+distances = list(map(int, input().split()))
+total = sum(distances)
+target = total / 2
 
-even = []
-odd = []
-
-for num in range(1, n * n + 1):
-    if num % 2 == 0:
-        even.append(num)
-    else:
-        odd.append(num)
-
-matrix = []
-e_ptr = 0
-o_ptr = 0
-
+current_sum = 0
 for i in range(n):
-    if i % 2 == 0:
-        row = []
-        for j in range(n // 2):
-            row.append(even[e_ptr])
-            e_ptr += 1
-        for j in range(n // 2):
-            row.append(even[e_ptr])
-            e_ptr += 1
-        matrix.append(row)
-    else:
-        row = []
-        for j in range(n 
+    current_sum += distances[i]
+    if current_sum >= target:
+        print(i + 1)
+        break
