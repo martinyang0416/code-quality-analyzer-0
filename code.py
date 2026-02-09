60ee0334e82edcc69274d53dfba096e448c26785
Analyzer <analyzer@test.com>
2026-02-09 14:58:10 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,13 @@
-import bisect
+MOD = 10**9 + 7
 
 class Solution:
-    def threeSumMulti(self, A: List[int], target: int) -> int:
-        MOD = 10**9 + 7
-        A.sort()
-        n = len(A)
-        count = 0
-        for i in range(n - 2):
-            for j in range(i + 1, n - 1):
-                required = target - A[i] - A[j]
-                left = bisect.bisect_left(A, required, j + 1, n)
-                right = bisect.bisect_right(A, required, j + 1, n)
-                count += right - left
-        return count % MOD
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        sub_sums = []
+        for i in range(n):
+            current_sum = 0
+            for j in range(i, n):
+                current_sum += nums[j]
+                sub_sums.append(current_sum)
+        sub_sums.sort()
+        total = sum(sub_sums[left-1:right])
+        return total % MOD
