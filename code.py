2cb6e0ae22ec63be8bbbdb449c1c4b94f3cc0564
Analyzer <analyzer@test.com>
2026-02-09 14:50:27 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,12 @@
-def breakPalindrome(palindrome):
-    n = len(palindrome)
-    if n == 1:
-        return ""
-    res = None
-    for i in range(n):
-        original = palindrome[i]
-        for c in 'abcdefghijklmnopqrstuvwxyz':
-            if c == original:
-                continue
-            new_pal = palindrome[:i] + c + palindrome[i+1:]
-            if new_pal != new_pal[::-1]:
-                if res is None or new_pal < res:
-                    res = new_pal
-    return res if res is not None else ""
+def integerReplacement(n):
+    steps = 0
+    while n > 1:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            if n == 3 or (n % 4 != 3):
+                n -= 1
+            else:
+                n += 1
+        steps += 1
+    return steps
