9527a49d3a09834053befbe9b76e104a341b9213
Analyzer <analyzer@test.com>
2026-02-09 15:00:46 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,15 @@
-from collections import deque
-
-def canVisitAllRooms(rooms):
-    visited = set([0])
-    queue = deque([0])
-    
-    while queue:
-        current_room = queue.popleft()
-        for key in rooms[current_room]:
-            if key not in visited:
-                visited.add(key)
-                queue.append(key)
-    return len(visited) == len(rooms)
+def lastStoneWeightII(stones):
+    total = sum(stones)
+    target = total // 2
+    dp = [False] * (target + 1)
+    dp[0] = True
+    for stone in stones:
+        for i in range(target, stone - 1, -1):
+            if dp[i - stone]:
+                dp[i] = True
+    max_sum = 0
+    for i in range(target, -1, -1):
+        if dp[i]:
+            max_sum = i
+            break
+    return total - 2 * max_sum
