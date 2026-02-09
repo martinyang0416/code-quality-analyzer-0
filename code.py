41c4356575ce59626cdcce636db5608fbe5f84bc
Analyzer <analyzer@test.com>
2026-02-09 14:15:53 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,18 @@
-def minSteps(n):
-    if n == 1:
-        return 0
-    res = 0
-    d = 2
-    while d * d <= n:
-        while n % d == 0:
-            res += d
-            n = n // d
-        d += 1
-    if n > 1:
-        res += n
-    return res
+from typing import List
+
+class Solution:
+    def partitionDisjoint(self, A: List[int]) -> int:
+        n = len(A)
+        max_left = [0] * n
+        max_left[0] = A[0]
+        for i in range(1, n):
+            max_left[i] = max(max_left[i-1], A[i])
+        
+        min_right = [0] * n
+        min_right[-1] = A[-1]
+        for i in range(n-2, -1, -1):
+            min_right[i] = min(min_right[i+1], A[i])
+        
+        for i in range(n-1):
+            if max_left[i] <= min_right[i+1]:
+          
