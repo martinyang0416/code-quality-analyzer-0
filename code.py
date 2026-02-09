3d6fd0d09189f234943a5b96c1e3b8dd15022719
Analyzer <analyzer@test.com>
2026-02-09 11:15:15 -0800

    Update

--- code.py
+++ code.py
@@ -1,3 +1,6 @@
-a1, a2 = map(int, input().split())
-reversed_a2 = int(str(a2)[::-1])
-print(a1 + reversed_a2)
+a1, a2, a3 = map(int, input().split())
+
+if a3 == 1:
+    print(a2)
+else:
+    print(a1 + a2 * (a3 - 1))
