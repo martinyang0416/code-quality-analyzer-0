bf512fb7db3cf7c172db2643afacffbd3d83fb86
Analyzer <analyzer@test.com>
2026-02-09 11:23:58 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,27 @@
-fixed_part1 = "What are you doing while sending "  # 33 characters
-fixed_part2 = " Are you busy? Will you send "     # 29 characters
-f0_str = "What are you doing at the end of the world? Are you busy? Will you save us?"  # length 75
+def putaway(A, B, T, X, Y, W, S):
+    # Compute max_X and max_Y
+    max_x = 0
+    if A > 0:
+        max_x = max(X)
+    else:
+        max_x = 0
+    max_y = 0
+    if B > 0:
+        max_y = max(Y)
+    else:
+        max_y = 0
 
-def solve():
-    import sys
-    input = sys.stdin.read().split()
-    q = int(input[0])
-    idx = 1
-    res = []
-    for _ in range(q):
-        n = int(input[idx])
-        k = int(input[idx+1])
-        idx +=2
-        
-        def get_char(n, k):
-            if n ==0
+    a = 0  # only_weak
+    b = 0  # only_small
+    c = 0  # either
+
+    for i in range(T):
+        w = W[i]
+        s = S[i]
+        can_weak = False
+        can_small = False
+        if A > 0:
+            if w < max_x:
+                can_weak = True
+        if B > 0:
+       
