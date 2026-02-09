52e734fc67ca344809a1531205c9cd625040ed14
Analyzer <analyzer@test.com>
2026-02-09 14:15:10 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,13 @@
-def minDeletionSize(A):
-    n = len(A)
-    if n <= 1:
+def minSteps(n):
+    if n == 1:
         return 0
-    m = len(A[0])
-    unfixed = set(range(n - 1))
-    deletions = 0
-    for col in range(m):
-        conflict = False
-        for i in unfixed:
-            if A[i][col] > A[i+1][col]:
-                conflict = True
-                break
-        if conflict:
-            deletions += 1
-        else:
-            to_remove = set()
-            for i in unfixed:
-                if A[i][col] < A[i+1][col]:
-                    to_re
+    res = 0
+    d = 2
+    while d * d <= n:
+        while n % d == 0:
+            res += d
+            n = n // d
+        d += 1
+    if n > 1:
+        res += n
+    return res
