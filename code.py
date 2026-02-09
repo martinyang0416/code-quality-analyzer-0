73e3e01220ce74789b06951c61538f8eac94d2bc
Analyzer <analyzer@test.com>
2026-02-09 14:26:48 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,15 @@
-def firstMissingPositive(nums):
-    n = len(nums)
-    for i in range(n):
-        while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
-            correct_pos = nums[i] - 1
-            nums[i], nums[correct_pos] = nums[correct_pos], nums[i]
-    for i in range(n):
-        if nums[i] != i + 1:
-            return i + 1
-    return n + 1
+def minFlipsMonoIncr(s: str) -> int:
+    n = len(s)
+    prefix_ones = [0] * (n + 1)
+    for i in range(1, n + 1):
+        prefix_ones[i] = prefix_ones[i - 1] + (1 if s[i - 1] == '1' else 0)
+    
+    suffix_zeros = [0] * (n + 1)
+    for i in range(n - 1, -1, -1):
+        suffix_zeros[i] = suffix_zeros[i + 1] + (1 if s[i] == '0' else 0)
+    
+    min_flips = float('inf')
+    for i in range(n + 1):
+        current = prefix_ones[i] + suffix_zeros[i]
+        if current < min_flips:
+            min_fli
