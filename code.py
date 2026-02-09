ea24d1d8bcdf483434e5061403eac0d1a4cafb6f
Analyzer <analyzer@test.com>
2026-02-09 14:26:01 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,10 @@
-import math
-from typing import List
-
-class Solution:
-    def numSquarefulPerms(self, A: List[int]) -> int:
-        A.sort()
-        n = len(A)
-        self.count = 0
-
-        def is_perfect_square(s):
-            root = math.isqrt(s)
-            return root * root == s
-
-        def backtrack(last, mask):
-            if mask == (1 << n) - 1:
-                self.count += 1
-                return
-            for i in range(n):
-                if mask & (1 << i):
-                    continue
-      
+def firstMissingPositive(nums):
+    n = len(nums)
+    for i in range(n):
+        while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+            correct_pos = nums[i] - 1
+            nums[i], nums[correct_pos] = nums[correct_pos], nums[i]
+    for i in range(n):
+        if nums[i] != i + 1:
+            return i + 1
+    return n + 1
