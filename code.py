5880f6f7ca375ae36c7e5b1a403dcec5519dd771
Analyzer <analyzer@test.com>
2026-02-09 13:22:19 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,6 @@
-import sys
-
-def main():
-    data = list(map(int, sys.stdin.read().split()))
-    ptr = 0
-    N, Q = data[ptr], data[ptr+1]
-    ptr += 2
-    A = data[ptr:ptr+N]
-    ptr += N
-    queries = data[ptr:ptr+Q]
-    s = set(A)
-    for x in queries:
-        print("YES" if x in s else "NO")
-
-if __name__ == "__main__":
-    main()
+t = int(input())
+vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+for _ in range(t):
+    word = input().strip()
+    collected = [c for c in word if c in vowels]
+    print(''.join(collected) if collected else "No")
