843899e91ccc601c310d1f018653ca37cf08d54d
Analyzer <analyzer@test.com>
2026-02-09 10:34:56 -0800

    Update

--- code.py
+++ code.py
@@ -1,4 +1,25 @@
-n = int(input())
-a = list(map(int, input().split()))
-unique_a = set(a)
-print(sum(unique_a))
+x, y, z = map(int, input().split())
+x1, y1, z1 = map(int, input().split())
+a1, a2, a3, a4, a5, a6 = map(int, input().split())
+
+total = 0
+
+# Check X-axis faces
+if x < 0:
+    total += a1
+elif x > x1:
+    total += a2
+
+# Check Y-axis faces
+if y < 0:
+    total += a3
+elif y > y1:
+    total += a4
+
+# Check Z-axis faces
+if z < 0:
+    total += a5
+elif z > z1:
+    total += a6
+
+print(total)
