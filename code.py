11b7a64d9aef4f6b1aefc201bb52af87e1eec8ed
Analyzer <analyzer@test.com>
2026-02-09 10:03:46 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,8 @@
-def main():
-    import sys
-    input = sys.stdin.read
-    data = input().split()
-    idx = 0
-    n = int(data[idx])
-    m = int(data[idx + 1])
-    idx += 2
-    for _ in range(m):
-        ai = int(data[idx])
-        bi = int(data[idx + 1])
-        ci = int(data[idx + 2])
-        idx += 3
-        if ai == 1:
-            print(0)
-        elif bi == n:
-            print(0)
-        else:
-            if ai < bi:
-                print(0)
-            else:
-                print(1)
+n = int(input())
+recipients = list(map(int, input().split()))
+counts = [0] * (n + 1)
 
-if __name__ == '__mai
+for r in recipients:
+    counts[r] += 1
+
+print(' '.join(map(str, counts[1:n+1])))
