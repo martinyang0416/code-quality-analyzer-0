4658f5f38c347bbfce26f7e446a09048e1952f0d
Analyzer <analyzer@test.com>
2026-02-09 10:15:52 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,17 @@
-import sys
-from collections import defaultdict
+n, m = map(int, input().split())
+matches = []
+for _ in range(n):
+    hi, mi, ni = map(int, input().split())
+    matches.append((hi, mi, ni))
 
-def main():
-    n = int(sys.stdin.readline())
-    vendors = list(map(int, sys.stdin.readline().split()))
-    m = int(sys.stdin.readline())
-    f1 = list(map(int, sys.stdin.readline().split()))
-    f2 = list(map(int, sys.stdin.readline().split()))
-    
-    first_counts = defaultdict(int)
-    second_total = defaultdict(int)
-    
-    for j in range(m):
-        a = f1[j]
-        b = f2[j]
-        first_counts[a] += 1
-        if a != b:
-            seco
+# Initialize DP table
+dp = [ [-float('inf')] * (m + 1) for _ in range(n + 1) ]
+dp[0][m] = 0  # Starting with m drinks, 0 matches played
+
+for i in range(1, n + 1):
+    hi, mi, ni = matches[i - 1]
+    for k_prev in range(m + 1):
+        if dp[i - 1][k_prev] != -float('inf'):
+            # Option 1: Use high
+            if k_prev >= 1:
+                new_k =
