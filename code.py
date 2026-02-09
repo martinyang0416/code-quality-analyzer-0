ecfdc39df3496ef717a2db385bd34f8f17ce1aa0
Analyzer <analyzer@test.com>
2026-02-09 15:11:06 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,17 @@
-# Read the number of test cases
-t = int(input())
-for _ in range(t):
-    # Read K, L, E
-    k, l, e = map(int, input().split())
-    # Read friends' ages
-    friends = list(map(int, input().split()))
-    total = e + sum(friends)
-    if l % total == 0:
-        print("YES")
-    else:
-        print("NO")
+import sys
+
+def main():
+    input = sys.stdin.readline
+    T = int(input())
+    for _ in range(T):
+        N, R = map(int, input().split())
+        canonical_map = {}
+        scores = {}
+        # Process first R submissions (registered teams)
+        for _ in range(R):
+            parts = input().strip().split()
+            ti = ' '.join(parts[:-1])
+            pi = int(parts[-1])
+            canonical = ''.join(sorted(ti))
+            canonical_map[canonical] = ti
+            scores[ti] = scor
