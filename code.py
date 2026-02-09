44feca9236c39f6d1f3dd0c28b7de1275176dc75
Analyzer <analyzer@test.com>
2026-02-09 15:16:18 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,19 @@
-MOD = 10**9 + 7
+import sys
+import math
 
 def main():
-    import sys
-    from collections import Counter
-    input = sys.stdin.read
-    data = input().split()
-    
-    N, K = int(data[0]), int(data[1])
-    A = list(map(int, data[2:2+N]))
-    
-    freq = list(Counter(A).values())
-    C = len(freq)
-    
-    if K >= C:
-        result = 1
-        for f in freq:
-            result = (result * (f + 1)) % MOD
-        print(result)
-    else:
-        dp = [0] * (K + 1)
-        dp[0] = 1
-        for f in freq:
-            for j i
+    data = sys.stdin.read().split()
+    t = int(data[0])
+    index = 1
+    for _ in range(t):
+        x = int(data[index])
+        y = int(data[index + 1])
+        index += 2
+        g = math.gcd(x, y)
+        lcm = x * y // g
+        a = lcm // x
+        b = lcm // y
+        print(a + b - 2)
+
+if __name__ == '__main__':
+    main()
