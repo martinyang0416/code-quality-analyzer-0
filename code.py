366a031b758f36604b2271b5cf1b3a2d5ac46430
Analyzer <analyzer@test.com>
2026-02-09 11:16:12 -0800

    Update

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 a1, a2, a3 = map(int, input().split())
-terms = [a1, a2]
-for _ in range(a3 - 1):
-    next_term = terms[-1] + terms[-2]
-    terms.append(next_term)
-print(terms[-1])
+steps = a3 - 1
+first, second = a1, a2
+for _ in range(steps):
+    next_term = first + second
+    first, second = second, next_term
+print(second)
