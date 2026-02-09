03fd69cd453b5be76383b51d1821f4fa00ac37db
Analyzer <analyzer@test.com>
2026-02-09 13:01:23 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,17 @@
-import sys
-import time
-import itertools
-from itertools import accumulate, product, permutations, combinations
-import collections
-from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
-from functools import lru_cache
-import math
-from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
-import fractions
-from typing import List, Tuple
-import numpy as np
-import random
-import heapq
-from heapq import *
-from dataclasses import dataclass
-
-import builtins
-import re
+T = int(input())
+for _ in range(T):
+    s = input().strip()
+    stack = []
+    valid = True
+    for c in s:
+        if c == '(':
+            stack.append(c)
+        elif c == ')':
+            if not stack:
+                valid = False
+                break
+            stack.pop()
+    if valid and not stack:
+        print("YES")
+    else:
+        print("NO")
