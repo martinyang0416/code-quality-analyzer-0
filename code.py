1c743743177b68155365db1c61c81a201825603c
Analyzer <analyzer@test.com>
2026-02-09 14:17:45 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,12 @@
-def isValidSerialization(preorder):
-    nodes = preorder.split(',')
-    slots = 1
-    for node in nodes:
-        if slots <= 0:
-            return False
-        slots -= 1
-        if node != '#':
-            slots += 2
-    return slots == 0
+def minFlips(a, b, c):
+    flips = 0
+    for i in range(31):
+        a_bit = (a >> i) & 1
+        b_bit = (b >> i) & 1
+        c_bit = (c >> i) & 1
+        if (a_bit | b_bit) != c_bit:
+            if c_bit == 1:
+                flips += 1
+            else:
+                flips += a_bit + b_bit
+    return flips
