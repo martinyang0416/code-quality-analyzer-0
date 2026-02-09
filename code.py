dcfacc7c59a04c3b00ea4250711b3765c6f58a2d
Analyzer <analyzer@test.com>
2026-02-09 13:38:06 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,3 @@
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    idx = 0
-    T = int(input[idx])
-    idx += 1
-    for _ in range(T):
-        N = int(input[idx])
-        K = int(input[idx + 1])
-        idx += 2
-        if K > 81:
-            print(-1)
-            continue
-        index = K - 1
-        a = (index // 9) + 1
-        idx_in_group = index % 9
-        sorted_b = []
-        if 0 != a:
-            sorted_b.append(0)
-        for x in range(1, 10):
-            if x != a:
-               
+n, *rest = map(int, open(0).read().split())
+a = rest[:n]
+print(sum(x - 1 for x in a))
