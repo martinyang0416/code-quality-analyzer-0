12168a395b416dfb790f0ebd7312f42a5320f80b
Analyzer <analyzer@test.com>
2026-02-09 10:07:32 -0800

    Update

--- code.py
+++ code.py
@@ -1,3 +1,16 @@
-n = int(input())
-student_ids = list(map(int, input().split()))
-print(len(set(student_ids)))
+M = int(input())
+N = int(input())
+mat = []
+for _ in range(M):
+    row = list(map(int, input().split()))
+    mat.append(row)
+
+# Transpose the matrix
+transposed = list(zip(*mat))
+
+# Reverse the order of the rows
+rotated = transposed[::-1]
+
+# Print the result
+for row in rotated:
+    print(' '.join(map(str, row)))
