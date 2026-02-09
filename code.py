183d61439370e2cbd1c5c0cdcbedab35bea26082
Analyzer <analyzer@test.com>
2026-02-09 10:53:11 -0800

    Update

--- code.py
+++ code.py
@@ -1,9 +1,6 @@
-def is_power_of_two(n):
-    return (n & (n - 1)) == 0 and n != 0
-
 a = int(input())
-bits = a.bit_length()
-if is_power_of_two(a):
-    print(bits - 1)
+e = a.bit_length() - 1
+if 2 ** e == a:
+    print(e)
 else:
-    print(bits)
+    print(e + 1)
