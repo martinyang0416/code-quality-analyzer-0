50fc474056ac61faf59cfabd42d0b455b2479da2
Analyzer <analyzer@test.com>
2026-02-09 14:54:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,15 @@
-def numTeams(rating):
-    n = len(rating)
-    total = 0
-    for j in range(1, n - 1):
-        left_small = left_large = 0
-        for i in range(j):
-            if rating[i] < rating[j]:
-                left_small += 1
-            elif rating[i] > rating[j]:
-                left_large += 1
-        
-        right_small = right_large = 0
-        for k in range(j + 1, n):
-            if rating[k] < rating[j]:
-                right_small += 1
-            elif rating[k] > rating[j]:
-                r
+def checkSubarraySum(nums, k):
+    if k == 0:
+        # Check for two consecutive zeros
+        for i in range(len(nums) - 1):
+            if nums[i] == 0 and nums[i+1] == 0:
+                return True
+        return False
+    else:
+        k_abs = abs(k)
+        mod_dict = {0: -1}  # To handle the case where subarray starts from index 0
+        prefix_mod = 0
+        for i in range(len(nums)):
+            prefix_mod = (prefix_mod + nums[i]) % k_abs
+            if prefix_mod in mod_dict:
+      
