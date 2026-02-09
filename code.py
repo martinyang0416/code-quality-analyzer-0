da112ed5bd4e451370190cf9cbbf3166e1d2de32
Analyzer <analyzer@test.com>
2026-02-09 13:49:10 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,9 @@
-class SegmentTree:
-    def __init__(self, data):
-        self.n = len(data)
-        self.size = 1
-        while self.size < self.n:
-            self.size <<= 1
-        self.tree = [ -float('inf') ] * (2 * self.size)
-        for i in range(self.n):
-            self.tree[self.size + i] = data[i]
-        for i in range(self.size -1, 0, -1):
-            self.tree[i] = max(self.tree[2*i], self.tree[2*i +1])
-    
-    def update(self, pos, value):
-        pos += self.size
-        self.tree[pos] = value
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input().strip()
+    first_gt = s.find('>')
+    last_lt = s.rfind('<')
+    left = first_gt if first_gt != -1 else float('inf')
+    right = (n - 1 - last_lt) if last_lt != -1 else float('inf')
+    print(min(left, right))
