bf379552819bd1c6a6cbf129c708b5b98d2a6624
Analyzer <analyzer@test.com>
2026-02-09 11:23:37 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,17 @@
-def main():
+fixed_part1 = "What are you doing while sending "  # 33 characters
+fixed_part2 = " Are you busy? Will you send "     # 29 characters
+f0_str = "What are you doing at the end of the world? Are you busy? Will you save us?"  # length 75
+
+def solve():
     import sys
-
-    fixed_A = "What are you doing while sending "  # length 34
-    fixed_B = " Are you busy? Will you send "      # length 29
-    f0 = "What are you doing at the end of the world? Are you busy? Will you save us?"  # length 75
-
-    q = int(sys.stdin.readline())
-    output = []
-
+    input = sys.stdin.read().split()
+    q = int(input[0])
+    idx = 1
+    res = []
     for _ in range(q):
-        n, k = map(int, sys.stdin.readline().split())
-        current_n = n
-        current_k = k
-        ans = '.'  # default if out of bounds
-
-        while current_n > 
+        n = int(input[idx])
+        k = int(input[idx+1])
+        idx +=2
+        
+        def get_char(n, k):
+            if n ==0
