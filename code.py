b6a187b62e966ff3d11825363b4ffc264314f3f4
Analyzer <analyzer@test.com>
2026-02-09 10:31:14 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,17 @@
-t = input().strip()
-result = []
-for char in t:
-    if char == 'A':
-        result.append('A')
-    elif char == 'B':
-        result.append('B')
-    elif char == 'D':
-        if result:
-            result.pop()
-print(''.join(result))
+def main():
+    import sys
+    n, k = map(int, sys.stdin.readline().split())
+    d = list(map(int, sys.stdin.readline().split()))
+    
+    # Compute prefix sums
+    prefix = [0] * (n + 1)
+    for i in range(1, n+1):
+        prefix[i] = prefix[i-1] + d[i-1]
+    
+    def is_feasible(mask):
+        # dp[i][j] means up to first i elements, j splits, meet the condition.
+        dp = [[False]*(k+1) for _ in range(n+1)]
+        dp[0][0] = True
+        
+        for i in range(1, n+1):
+            for j 
