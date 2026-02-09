f685d9469a821050fb4c8773cc1787370770d2eb
Analyzer <analyzer@test.com>
2026-02-09 13:53:41 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,5 @@
-m = list(map(int, input().split()))
-w = list(map(int, input().split()))
-hs, hu = map(int, input().split())
-
-total_score = 0
-
-for i in range(5):
-    x = 500 * (i + 1)
-    mi = m[i]
-    wi = w[i]
-    part1 = 0.3 * x
-    part2 = x * (1 - mi / 250) - 50 * wi
-    total_score += max(part1, part2)
-
-total_score += 100 * hs - 50 * hu
-print(int(total_score))
+n, k = map(int, input().split())
+w_list = list(map(int, input().split()))
+total_pockets = sum((w + k - 1) // k for w in w_list)
+days = (total_pockets + 1) // 2
+print(days)
