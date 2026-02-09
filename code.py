e38de6dcf34d6441442c0f9e3116f5e43fd78c90
Analyzer <analyzer@test.com>
2026-02-09 13:55:36 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,5 @@
 n = int(input())
-k = int(input())
-arr = [int(input()) for _ in range(n)]
-arr.sort()
-
-min_diff = float('inf')
-for i in range(n - k + 1):
-    current_diff = arr[i + k - 1] - arr[i]
-    if current_diff < min_diff:
-        min_diff = current_diff
-
-print(min_diff)
+total = 0
+for a in range(1, n):
+    total += (n - 1) // a
+print(total)
