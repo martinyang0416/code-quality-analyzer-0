d8335ce04d794961bb8199c52c2a6d8796257d09
Analyzer <analyzer@test.com>
2026-02-09 10:19:44 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,19 @@
-import sys
-from collections import defaultdict
+n = int(input())
+s = input().strip()
 
-def main():
-    m = int(sys.stdin.readline())
-    edges = []
-    outgoing = defaultdict(list)
-
-    for idx in range(m):
-        u, v = map(int, sys.stdin.readline().split())
-        edges.append((u, v))
-        outgoing[u].append(idx)
-    
-    max_degree = 0
-    for u in outgoing:
-        current = len(outgoing[u])
-        if current > max_degree:
-            max_degree = current
-    
-    k = max_degree
-    colors = [0] * m
-
-    for u in outgoing:
+herbs = [i for i, c in enumerate(s) if c == 'H']
+if len(herbs) < 5:
+    print("impossible")
+else:
+    herb_set = set(herbs)
+    found = False
+    for i in range(len(herbs)):
+        for j in range(i + 1, len(herbs)):
+            a0 = herbs[i]
+            a1 = herbs[j]
+            d = a1 - a0
+            a2 = a0 + 2 * d
+            a3 = a0 + 3 * d
+            a4 = a0 + 4 * d
+            if a2 in herb_set and a3 in herb_set and a4 in herb_set:
+                
