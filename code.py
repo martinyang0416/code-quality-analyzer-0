917e2c1c33068e49fac3d9865eb3c27d740dadda
Analyzer <analyzer@test.com>
2026-02-09 10:13:22 -0800

    Update

--- code.py
+++ code.py
@@ -1,9 +1,22 @@
-k = int(input())
-nums = list(map(int, input().split()))
+target, n = map(int, input().split())
 
-s = {0}
-for num in nums:
-    for current in list(s):
-        s.add(current + num)
+# Calculate the sum of squares from 1 to n
+sum_squares = n * (n + 1) * (2 * n + 1) // 6
 
-print(len(s))
+if target > sum_squares:
+    print(-1)
+else:
+    remaining = target
+    result = []
+    for i in range(n, 0, -1):
+        square = i * i
+        if square <= remaining:
+            result.append(i)
+            remaining -= square
+            if remaining == 0:
+                break
+    if remaining != 0:
+        print(-1)
+    else:
+        print(len(result))
+        pri
