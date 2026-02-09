91aa1fc0aa5a4ab332c21e47d1e613bdc27f744a
Analyzer <analyzer@test.com>
2026-02-09 14:37:09 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,7 @@
-def longestPrefix(s: str) -> str:
-    n = len(s)
-    pi = [0] * n
-    for i in range(1, n):
-        j = pi[i-1]
-        while j > 0 and s[i] != s[j]:
-            j = pi[j-1]
-        if s[i] == s[j]:
-            j += 1
-        pi[i] = j
-    max_len = pi[-1]
-    return s[:max_len] if max_len > 0 else ""
+class Solution:
+    def checkIfCanBreak(self, s1: str, s2: str) -> bool:
+        s1_sorted = sorted(s1)
+        s2_sorted = sorted(s2)
+        condition1 = all(c1 >= c2 for c1, c2 in zip(s1_sorted, s2_sorted))
+        condition2 = all(c2 >= c1 for c1, c2 in zip(s1_sorted, s2_sorted))
+        return condition1 or condition2
