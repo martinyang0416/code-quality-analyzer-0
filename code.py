c0ab71ab81035b90cdaafa38150d54b33ebaa9ce
Analyzer <analyzer@test.com>
2026-02-09 10:11:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,27 +1,17 @@
-import math
-
-def compute_gcd(arr):
-    current_gcd = arr[0]
-    for num in arr[1:]:
-        current_gcd = math.gcd(current_gcd, num)
-        if current_gcd == 1:
-            break
-    return current_gcd
-
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    n = int(input[0])
-    a = list(map(int, input[1:n+1]))
-    
-    if n == 0:
-        print(0)
-        return
-    
-    g = compute_gcd(a)
-    
-    b = [num // g for num in a]
-    
-    if len(b) < 2:
-        print(0)
-        return
+def build_transition(pattern):
+    len_p = len(pattern)
+    failure = [0] * len_p
+    for i in range(1, len_p):
+        j = failure[i-1]
+        while j > 0 and pattern[i] != pattern[j]:
+            j = failure[j-1]
+        if pattern[i] == pattern[j]:
+            j += 1
+        failure[i] = j
+    transition = [[0]*26 for _ in range(len_p + 1)]
+    for state in range(len_p + 1):
+        for c in range(26):
+            char = chr(ord('a') + c)
+            j = state
+            while True:
+       
