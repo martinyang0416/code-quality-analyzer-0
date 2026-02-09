ef6d94960c7388c215c76d3f9a3f55fe1692df4f
Analyzer <analyzer@test.com>
2026-02-09 11:08:26 -0800

    Update

--- code.py
+++ code.py
@@ -1,2 +1,11 @@
-a = int(input())
-print(1 if a % 2 != 0 else 0)
+def main():
+    import sys
+    N, M = map(int, sys.stdin.readline().split())
+    bits = bin(N).count('1')
+    if M >= bits and (M - bits) % 2 == 0:
+        print("YES")
+    else:
+        print("NO")
+
+if __name__ == "__main__":
+    main()
