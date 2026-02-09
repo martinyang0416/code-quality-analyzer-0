3de52571be1e53f43f274b4b25af7b989a740b90
Analyzer <analyzer@test.com>
2026-02-09 10:09:51 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,25 @@
-n = int(input())
-L = list(map(int, input().split()))
-path = [0] + L + [0]
+def main():
+    import sys
+    input = sys.stdin.read().split()
+    ptr = 0
+    N = int(input[ptr])
+    ptr += 1
+    M = float(input[ptr])
+    ptr += 1
 
-original = 0
-for i in range(1, len(path)):
-    original += abs(path[i] - path[i-1])
+    cylinders = []
 
-for i in range(1, n+1):
-    pre = path[i-1]
-    curr = path[i]
-    next_val = path[i+1]
-    a = abs(curr - pre)
-    b = abs(next_val - curr)
-    c = abs(next_val - pre)
-    new_dist = original - a - b + c
-    print(new_dist)
+    for _ in range(N):
+        K = int(input[ptr])
+        ptr += 1
+        for i in range(K):
+            S = float(input[ptr])
+            ptr += 1
+            H = float(input[ptr])
+            ptr += 1
+            cylinders.append((S, H))
+
+    # Sort by base area (S)
+    cylinders.sort(key=lambda x: x[0])
+
+    total_height
