7d5ecaf48bac961c83c8267311e95acebe39e930
Analyzer <analyzer@test.com>
2026-02-09 15:10:21 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,12 @@
-import sys
-
-def main():
-    input = sys.stdin.read
-    data = input().split()
-    T = int(data[0])
-    cases = data[1:T+1]
-    for s in cases:
-        N = len(s)
-        forward = [1] * (N + 1)
-        for i in range(1, N + 1):
-            prev_char = s[i-1]
-            if prev_char == '<':
-                forward[i] = forward[i-1] + 1
-            elif prev_char == '=':
-                forward[i] = forward[i-1]
-            else:
-                forward[i] = 1
-        backward = [1] * (N + 1)
-   
+# Read the number of test cases
+t = int(input())
+for _ in range(t):
+    # Read K, L, E
+    k, l, e = map(int, input().split())
+    # Read friends' ages
+    friends = list(map(int, input().split()))
+    total = e + sum(friends)
+    if l % total == 0:
+        print("YES")
+    else:
+        print("NO")
