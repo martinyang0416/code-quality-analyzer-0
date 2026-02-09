7ec06b8481fc5e709a29120a3415a13bf44c23e5
Analyzer <analyzer@test.com>
2026-02-09 14:40:02 -0800

    Update

--- code.py
+++ code.py
@@ -1,9 +1,10 @@
-from collections import Counter
-
-def minSteps(s: str, t: str) -> int:
-    count_s = Counter(s)
-    count_t = Counter(t)
-    steps = 0
-    for c in 'abcdefghijklmnopqrstuvwxyz':
-        steps += max(0, count_s[c] - count_t[c])
-    return steps
+def countBattleships(board):
+    count = 0
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 'X':
+                top_ok = (i == 0) or (board[i-1][j] != 'X')
+                left_ok = (j == 0) or (board[i][j-1] != 'X')
+                if top_ok and left_ok:
+                    count += 1
+    return count
