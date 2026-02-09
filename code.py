bb8bb85571ce07aaed445fe9d3a133a765648840
Analyzer <analyzer@test.com>
2026-02-09 10:05:34 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,18 @@
 n = int(input())
-m = 2 * n - 2
-total = 0
-
-for i in range(m - n + 1):
-    # Calculate the number of edges
-    edges = 0
-    if i > 0:
-        edges += 1
-    if i + n < m:
-        edges += 1
-    
-    # Calculate the number of remaining lamps
-    remaining = m - n - edges
-    ways = 4 * (3 ** edges) * (4 ** remaining)
-    total += ways
-
-print(total)
+x, y = 0, 0
+directions = []
+for _ in range(n):
+    a, b = map(int, input().split())
+    new_x_plus = x + a
+    new_y_plus = y + b
+    d_plus = abs(new_x_plus) + abs(new_y_plus)
+    new_x_minus = x - a
+    new_y_minus = y - b
+    d_minus = abs(new_x_minus) + abs(new_y_minus)
+    if d_plus < d_minus:
+        directions.append(1)
+        x, y = new_x_plus, new_y_plus
+    else:
+        directions.append(-1)
+        x, y = new_x_minus, new_y_minus
+print(' '.join(map(str, directions))
