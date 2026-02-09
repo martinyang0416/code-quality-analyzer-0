48fc8aa873c52ae777feb5abce49af1984ce005a
Analyzer <analyzer@test.com>
2026-02-09 10:49:08 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,6 @@
-def solve(a):
-    binary = bin(a)[2:].zfill(6)
-    bits = list(binary)
-    # The permutation indices are [0,5,3,2,4,1]
-    new_bits = [bits[0], bits[5], bits[3], bits[2], bits[4], bits[1]]
-    new_binary = ''.join(new_bits)
-    return int(new_binary, 2)
-
 a = int(input())
-print(solve(a))
+binary = bin(a)[2:].zfill(6)
+bits = list(map(int, binary))
+output_bits = [bits[0], bits[5], bits[3], bits[2], bits[4], bits[1]]
+result = int(''.join(map(str, output_bits)), 2)
+print(result)
