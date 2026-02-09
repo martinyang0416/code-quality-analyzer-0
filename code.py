c19e3bfbd6877d2473913e4c1df05b1d1b147e94
Analyzer <analyzer@test.com>
2026-02-09 14:23:17 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,19 @@
-def search(nums, target):
-    left, right = 0, len(nums) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if nums[mid] == target:
-            return mid
-        if nums[left] <= nums[mid]:
-            if nums[left] <= target < nums[mid]:
-                right = mid - 1
-            else:
-                left = mid + 1
-        else:
-            if nums[mid] < target <= nums[right]:
-                left = mid + 1
-            else:
-                right = mid - 1
-    return -1
+def mergeStones(stones, K):
+    n = len(stones)
+    if (n - 1) % (K - 1) != 0:
+        return -1
+    
+    prefix = [0] * (n + 1)
+    for i in range(n):
+        prefix[i + 1] = prefix[i] + stones[i]
+    
+    dp = [[float('inf')] * n for _ in range(n)]
+    for i in range(n):
+        dp[i][i] = 0
+    
+    for l in range(2, n + 1):
+        for i in range(n - l + 1):
+            j = i + l - 1
+            total = prefix[j + 1] - prefix[i]
+            for k in range(i, j):
+                left_len = k 
