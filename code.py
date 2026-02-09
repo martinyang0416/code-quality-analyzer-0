66551cbbc870d91612783ab37ab197ec80116682
Analyzer <analyzer@test.com>
2026-02-09 13:23:09 -0800

    Update

--- code.py
+++ code.py
@@ -1,2 +1,24 @@
-a, b = map(int, input().split())
-print("Even" if a % 2 == 0 or b % 2 == 0 else "Odd")
+import sys
+from collections import defaultdict
+
+def main():
+    N = int(sys.stdin.readline())
+    counts = defaultdict(int)
+    initial_max = 0
+
+    for _ in range(N):
+        a, b = map(int, sys.stdin.readline().split())
+        k = a + b
+        counts[k] += 1
+        if k > initial_max:
+            initial_max = k
+
+    current_max = initial_max
+    k = 0
+
+    while k <= current_max:
+        cnt = counts.get(k, 0)
+        carry = cnt // 2
+        rem = cnt % 2
+        counts[k] = rem
+        i
