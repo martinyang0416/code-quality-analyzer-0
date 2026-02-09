cb3d09b3f75eadf7a915e701b8389e6501c0a7a6
Analyzer <analyzer@test.com>
2026-02-09 10:32:31 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,6 @@
+MOD = 10**9 + 7
+inv2 = 500000004  # Modular inverse of 2 under MOD
+
 n = int(input())
-s = list(map(int, input().split()))
-max_dp = {}
-max_sum = 0
-
-for i in range(n):
-    k = s[i] - (i + 1)
-    if k in max_dp:
-        current = max_dp[k] + s[i]
-    else:
-        current = s[i]
-    if k in max_dp:
-        if current > max_dp[k]:
-            max_dp[k] = current
-    else:
-        max_dp[k] = current
-    if current > max_sum:
-        max_sum = current
-
-print(max_sum)
+res = (n * inv2) % MOD
+print('\n'.join([str(res)] * n))
