b50add2d5d959ab44e4ecfe8c46b596b1b8953a9
Analyzer <analyzer@test.com>
2026-02-09 12:15:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,14 @@
-def f(a):
-  res = 1
-  for i in range(1, 1+a):
-    res *= i
-  return res
+def f(l):
+    for i in range(len(l)-1):
+        if l[i] > l[i+1]:
+            break
+    #print(i+1)
+    if sorted(l[i+1:],reverse=True) == l[i+1:]:return 0
+    return 1
+
+M = 10**9 + 7
+R = lambda: map(int, input().split())
 n = int(input())
-print(f(n) * f(n//2 - 1) ** 2 // f(n//2) ** 2 // 2)
+L = list(R())
+if len(set(L)) != n:print("NO")
+else:print("YNEOS"[f(L)::2])
