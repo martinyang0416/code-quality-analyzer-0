069f925a588890bbac2f4806d20d09fbdff4bc83
Analyzer <analyzer@test.com>
2026-02-09 13:27:39 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,17 @@
-import sys
-from collections import defaultdict
+n = int(input())
+s = input().strip()
 
-def main():
-    n, m = map(int, sys.stdin.readline().split())
-    if m % 2 != 0:
-        print("No solution")
-        return
-    
-    edges = []
-    adj = defaultdict(list)
-    for _ in range(m):
-        u, v = map(int, sys.stdin.readline().split())
-        edges.append((u, v))
-        adj[u].append((v, len(edges)-1))
-        adj[v].append((u, len(edges)-1))
-    
-    used = [False] * m
-    ans = []
-    visited = [False] * (n + 1)
-    parent = {}
-  
+for i in range(n):
+    if s[i] == '*':
+        max_d = (n - 1 - i) // 4
+        for d in range(1, max_d + 1):
+            valid = True
+            for k in range(5):
+                pos = i + k * d
+                if pos >= n or s[pos] != '*':
+                    valid = False
+                    break
+            if valid:
+                print("yes")
+                exit()
+print("no")
