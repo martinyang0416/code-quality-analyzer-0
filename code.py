27185e659d964307f1e435f115ae1b447b2c1f45
Analyzer <analyzer@test.com>
2026-02-09 13:33:05 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,14 @@
 import sys
-from collections import defaultdict
 
-def main():
-    kx, ky = map(int, sys.stdin.readline().split())
-    rook_positions = set()
-    rooks = {}
-    rows = defaultdict(int)
-    cols = defaultdict(int)
-    for i in range(666):
-        x, y = map(int, sys.stdin.readline().split())
-        rooks[i+1] = (x, y)
-        rook_positions.add((x, y))
-        rows[x] += 1
-        cols[y] += 1
-
-    current_king = (kx, ky)
-    for _ in range(2000):
-        kx, ky = current_king
-        best_move = N
+t = int(sys.stdin.readline())
+for _ in range(t):
+    n = int(sys.stdin.readline())
+    max_l = 0
+    min_r = float('inf')
+    for _ in range(n):
+        l, r = map(int, sys.stdin.readline().split())
+        if l > max_l:
+            max_l = l
+        if r < min_r:
+            min_r = r
+    print(max(0, max_l - min_r))
