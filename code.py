b550881bea50faaeb1790a595262c9f655a67f4e
Analyzer <analyzer@test.com>
2026-02-09 12:53:02 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,25 @@
-def solve(arr):
-    for i in range(0,len(arr)-1):
-        x = arr[i+1].find(arr[i])
-       # print(x)
-        if x == -1:
-            return 0
-    return 1
-n=int(input())
-arr = []
-while n:
-    s = str(input())
-    arr.append(s)
-    n=n-1
-arr.sort(key=len)
-flag = solve(arr)
-if flag:
-    print("YES")
-    for i in arr:
-        print(i)
-else:
-    print("NO")
+def readln(): return map(int, input().rstrip().split())
+
+
+data = {}
+n = int(input())
+for i in range(1, n + 1):
+    data[i] = []
+for i in range(0, n - 1):
+    u, v = readln()
+    data[u].append(v)
+    data[v].append(u)
+
+l = [0, 0]
+visited = [False] * (n + 1)
+
+stk = [(1, 0)]
+while stk:
+    u = stk.pop()
+    visited[u[0]] = True
+    l[u[1]] += 1
+    for i in data[u[0]]:
+        if not visited[i]:
+            stk.append((i, 1 - u[1]))
+
+print(l[0] * l[1] - (n - 1))
