8f06b8c9572237ad86e5fc2b745dd85967b5ea11
Analyzer <analyzer@test.com>
2026-02-09 15:05:30 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,23 @@
-class FenwickTree:
-    def __init__(self, size):
-        self.n = size
-        self.tree = [0] * (self.n + 1)  # 1-based indexing
+import sys
+from collections import defaultdict
 
-    def update(self, idx, delta):
-        while idx <= self.n:
-            self.tree[idx] = (self.tree[idx] + delta) % 2
-            idx += idx & -idx
-
-    def query(self, idx):
-        res = 0
-        while idx > 0:
-            res = (res + self.tree[idx]) % 2
-            idx -= idx & -idx
-        return res
-
-primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+def main():
+    input = sys.stdin.read().split()
+    ptr = 0
+    T = int(input[ptr])
+    ptr += 1
+    for _ in range(T):
+        n = int(input[ptr])
+        m = int(input[ptr+1])
+        ptr +=2
+        if m == 0:
+            print(0)
+            ptr += 2*m
+            continue
+        adj = defaultdict(set)
+        edges = []
+        for __ in range(m):
+            u = int(input[ptr])
+            v = int(input[ptr+1])
+            ptr +=2
+         
