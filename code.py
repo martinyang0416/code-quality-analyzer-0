e90b45c8bd78938cb4857d1cd3b43943aedf99a1
Analyzer <analyzer@test.com>
2026-02-09 10:29:05 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,26 @@
-T = int(input())
-for _ in range(T):
-    N = int(input())
-    for i in range(1, N + 1):
-        numbers = list(range(1, i + 1))
-        line = ' '.join(map(str, numbers))
-        print(line)
+import bisect
+
+def main():
+    import sys
+    input = sys.stdin.read().split()
+    ptr = 0
+    d = int(input[ptr])
+    ptr +=1
+    n = int(input[ptr])
+    m = int(input[ptr+1])
+    ptr +=2
+
+    points = []
+    for _ in range(d):
+        x = int(input[ptr])
+        y = int(input[ptr+1])
+        points.append( (x, y) )
+        ptr +=2
+
+    cnt_left = int(input[ptr])
+    cnt_right = int(input[ptr+1])
+    cnt_top = int(input[ptr+2])
+    cnt_bottom = int(input[ptr+3])
+    ptr +=4
+
+    # Create set of
