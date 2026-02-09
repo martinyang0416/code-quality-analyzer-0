c97cab5e4a3e60a4d80f8e2367c3124a13d9d2e1
Analyzer <analyzer@test.com>
2026-02-09 15:02:50 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,16 @@
-def minRemoveToMakeValid(s):
-    stack = []
-    remove = set()
-    for i, c in enumerate(s):
-        if c == '(':
-            stack.append(i)
-        elif c == ')':
-            if stack:
-                stack.pop()
-            else:
-                remove.add(i)
-    # Add remaining unmatched '(' indices to remove set
-    remove.update(stack)
-    # Build the result string
-    return ''.join([c for i, c in enumerate(s) if i not in remove])
+class SegmentTree:
+    def __init__(self, data):
+        self.n = len(data)
+        self.tree = [0] * (4 * self.n)
+        self.build(0, self.n - 1, 1, data)
+    
+    def build(self, l, r, node, data):
+        if l == r:
+            self.tree[node] = 1 << (ord(data[l]) - ord('a'))
+            return
+        mid = (l + r) // 2
+        self.build(l, mid, 2 * node, data)
+        self.build(mid + 1, r, 2 * node + 1, data)
+        self.tree[node] = self.tree[2 * node] | self.tree[2 * node + 1]
+    
+ 
