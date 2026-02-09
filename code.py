1d6291fa97eeffafee192c66c0cb4c647c5c07fd
Analyzer <analyzer@test.com>
2026-02-09 14:31:17 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,19 @@
-def baseNeg2(N):
-    if N == 0:
-        return "0"
-    res = []
-    while N != 0:
-        remainder = N % (-2)
-        N = N // (-2)
-        if remainder < 0:
-            remainder += 2
-            N += 1
-        res.append(str(remainder))
-    return ''.join(reversed(res))
+from collections import deque
+
+def racecar(target):
+    queue = deque([(0, 1, 0)])
+    visited = set([(0, 1)])
+    
+    while queue:
+        pos, speed, steps = queue.popleft()
+        
+        if pos == target:
+            return steps
+        
+        # Action A: Accelerate
+        new_pos = pos + speed
+        new_speed = speed * 2
+        if (new_pos, new_speed) not in visited:
+            # Prune positions that are too far from the target
+            if abs(new_pos) <= 2 * target:
+         
