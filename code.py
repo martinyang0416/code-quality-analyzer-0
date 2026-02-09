09608ff76b26e69149322b01fcf54642c0ae5b1d
Analyzer <analyzer@test.com>
2026-02-09 11:28:50 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,20 @@
-import bisect
-
 def putaway(A, B, T, X, Y, W, S):
-    X_sorted = sorted(X)
-    Y_sorted = sorted(Y)
-    
-    can_weak_list = [False] * T
-    can_small_list = [False] * T
-    
+    can_weak = [False] * T
+    can_small = [False] * T
+
     for i in range(T):
-        w = W[i]
-        s = S[i]
-        # Check can_weak
-        idx = bisect.bisect_right(X_sorted, w)
-        can_weak = (idx < len(X_sorted))
-        can_weak_list[i] = can_weak
-        # Check can_small
-        idx2 = bisect.bisect_right(Y_sorted, s)
-        can_small = (idx2 < len(Y_sorted))
-   
+        # Check can_weak[i]
+        for x in X:
+            if x > W[i]:
+                can_weak[i] = True
+                break
+        # Check can_small[i]
+        for y in Y:
+            if y > S[i]:
+                can_small[i] = True
+                break
+
+    # Check if any toy cannot be handled by any robot
+    for i in range(T):
+        if not (can_weak[i] or can_small[i]):
+ 
