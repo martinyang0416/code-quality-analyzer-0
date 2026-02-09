48d45c213cac25c0138749338752dbef4824d23c
Analyzer <analyzer@test.com>
2026-02-09 10:17:19 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,10 @@
-n, f = map(int, input().split())
-s = list(map(int, input().split()))
-t = list(map(int, input().split()))
+a, b = map(int, input().split())
 
-# Calculate Alice's score
-alice_total = 0
-alice_time = 0
-for i in range(n):
-    alice_time += t[i]
-    alice_total += s[i] - f * alice_time
+def minimal_capacitors(a, b):
+    sum_q = 0
+    while b != 0:
+        sum_q += a // b
+        a, b = b, a % b
+    return sum_q
 
-# Calculate Bob's score
-bob_total = 0
-bob_time = 0
-for i in range(n-1, -1, -1):
-    bob_time += t[i]
-    bob_total += s[i] - f * bob_time
-
-# Compare the totals
-if alice_total > bob_total:
-    print("Alice")
-elif bob_total > alice_total:
-    print("Bob")
+print(minimal_capacitors(a, b))
