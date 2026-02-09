ddc1b764ad2ea90e97713f7cc95d8b12e815c86f
Analyzer <analyzer@test.com>
2026-02-09 13:30:39 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,20 @@
-import sys
-
-def main():
-    input = sys.stdin.read().split()
-    ptr = 0
-    N = int(input[ptr])
-    ptr += 1
-    Q = int(input[ptr])
-    ptr += 1
-    a = list(map(int, input[ptr:ptr+N]))
-    ptr += N
-    
-    pre_sum = [0] * (N + 1)
-    for i in range(1, N + 1):
-        pre_sum[i] = pre_sum[i-1] + a[i-1]
-    
-    for _ in range(Q):
-        L = int(input[ptr])
-        ptr += 1
-        R = int(input[ptr])
-        ptr += 1
-        
-        sum_res = pre_sum[R] - pre_sum[L-1]
-        if L == R:
-   
+n = int(input())
+shop_masks = []
+for _ in range(n):
+    f = list(map(int, input().split()))
+    mask = 0
+    for j in range(10):
+        if f[j]:
+            mask |= 1 << j
+    shop_masks.append(mask)
+p = []
+for _ in range(n):
+    p.append(list(map(int, input().split())))
+max_total = -float('inf')
+for joisino_mask in range(1, 1 << 10):
+    total = 0
+    for i in range(n):
+        overlap = joisino_mask & shop_masks[i]
+        c = bin(overlap).count('1')
+        total += p[i][c]
+    if total > ma
