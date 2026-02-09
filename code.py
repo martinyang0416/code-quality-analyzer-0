4ed6d16d646d7048a157ab9761678afe204355ae
Analyzer <analyzer@test.com>
2026-02-09 10:39:46 -0800

    Update

--- code.py
+++ code.py
@@ -1,3 +1,20 @@
-t = input().strip()
-result = '9' * len(t)
-print(result)
+s = input().strip()
+
+vowels = {'a', 'e', 'i', 'o', 'u'}
+
+non_vowels = []
+for c in s:
+    if c.lower() not in vowels:
+        non_vowels.append(c)
+reversed_non_vowels = non_vowels[::-1]
+
+result = []
+ptr = 0
+for c in s:
+    if c.lower() in vowels:
+        result.append(c)
+    else:
+        result.append(reversed_non_vowels[ptr])
+        ptr += 1
+
+print(''.join(result))
