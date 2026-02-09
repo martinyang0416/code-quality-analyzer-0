8a5d8ff28724acfa8759d96d0ef7dde52bcde1b8
Analyzer <analyzer@test.com>
2026-02-09 12:14:38 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,13 @@
-import sys
-import time
-import itertools
-from itertools import accumulate, product, permutations, combinations
-import collections
-from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
-from functools import lru_cache
-import math
-from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
-import fractions
-from typing import List, Tuple
-import numpy as np
-import random
-import heapq
-from heapq import *
-from dataclasses import dataclass
+def main():
+    n = int(input())
+    s = input()
 
-import builtins
-import re
+    try:
+        index = next(i for i in range(n - 1) if s[i] > s[i + 1])
+        print('YES\n%d %d' % (index + 1, index + 2))
+    except StopIteration:
+        print('NO')
+
+
+if __name__ == '__main__':
+    main()
