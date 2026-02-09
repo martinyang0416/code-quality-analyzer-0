8c4d7474cd9c3e3ce0ce9ebd034fd6a515002ac7
Analyzer <analyzer@test.com>
2026-02-09 14:02:01 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,7 @@
-s = input().strip()
-t = input().strip()
-
-from collections import Counter
-
-count_s = Counter(s)
-count_t = Counter(t)
-
-# Check if t can be formed by deleting characters (subset check)
-for char in count_t:
-    if count_t[char] > count_s.get(char, 0):
-        print("need tree")
+a, b, c = map(int, input().split())
+for x in range(c // a + 1):
+    remainder = c - a * x
+    if remainder >= 0 and remainder % b == 0:
+        print("Yes")
         exit()
-
-# Check if s and t are anagrams
-if sorted(s) == sorted(t):
-    print("array")
-else:
-    # Check if t is a subsequence of s
-    i = j = 0
-    n, m = len(s), len(t)
-    while i < n and j < m:
-        if s[i] == 
+print("No")
