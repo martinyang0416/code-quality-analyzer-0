55c6736e7fd49e842eb8912bf07c475185bd51c3
Analyzer <analyzer@test.com>
2026-02-09 14:24:00 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,16 @@
-def mergeStones(stones, K):
-    n = len(stones)
-    if (n - 1) % (K - 1) != 0:
-        return -1
-    
-    prefix = [0] * (n + 1)
-    for i in range(n):
-        prefix[i + 1] = prefix[i] + stones[i]
-    
-    dp = [[float('inf')] * n for _ in range(n)]
-    for i in range(n):
-        dp[i][i] = 0
-    
-    for l in range(2, n + 1):
-        for i in range(n - l + 1):
-            j = i + l - 1
-            total = prefix[j + 1] - prefix[i]
-            for k in range(i, j):
-                left_len = k 
+from typing import List
+
+class Solution:
+    def movesToMakeZigzag(self, nums: List[int]) -> int:
+        def calculate(start):
+            arr = nums.copy()
+            moves = 0
+            n = len(arr)
+            for i in range(start, n, 2):
+                current = nums[i]
+                if i > 0:
+                    required = current - 1
+                    if arr[i-1] > required:
+                        moves += arr[i-1] - required
+                        arr[i-1] = required
+          
