a25f97d71ee8ae655c9db135dd4f46f10d862d08
Analyzer <analyzer@test.com>
2026-02-09 10:12:53 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,9 @@
-import sys
-from collections import deque
+k = int(input())
+nums = list(map(int, input().split()))
 
-MOD = 10**9 + 7
+s = {0}
+for num in nums:
+    for current in list(s):
+        s.add(current + num)
 
-def main():
-    n = int(sys.stdin.readline())
-    values = list(map(int, sys.stdin.readline().split()))
-    edges = [[] for _ in range(n+1)]  # 1-based indexing
-    for _ in range(n-1):
-        a, b = map(int, sys.stdin.readline().split())
-        edges[a].append(b)
-        edges[b].append(a)
-    
-    # BFS to find parent and order
-    visited = [False] * (n + 1)
-    parent = [0] * (n + 1)
-    order = []
-    q = deque()
-    q.append(1)
- 
+print(len(s))
