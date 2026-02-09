8f07d885695e839a7b867bbe76561cd918155d81
Analyzer <analyzer@test.com>
2026-02-09 14:09:50 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,9 @@
-class DSU:
-    def __init__(self, n):
-        self.parent = list(range(n+1))
-        self.size = [1] * (n+1)
-        self.undo_steps = dict()  # Maps day to list of undo steps
+import sys
 
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, x, y, day):
-        x_root = self.find(x)
-        y_root = self.find(y)
-        if x_root == y_root:
-            return
-        # Ensure x_root is the larger tree
+t = int(sys.stdin.readline())
+for _ in range(t):
+    n = int(sys.stdin.readline())
+    a = list(map(int, sys.stdin.readline().split()))
+    s = sum(a)
+    r = s % n
+    print(r * (n - r))
