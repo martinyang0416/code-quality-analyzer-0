f3ebe21832fe24f22ecb2fea2556066167a18c41
Analyzer <analyzer@test.com>
2026-02-09 12:25:08 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,19 @@
-def Fact(n):
-  res = 1 
-  while n > 0 :
-    res *= n
-    n -= 1
-  return res
+import sys
+import time
+import itertools
+from itertools import accumulate, product, permutations, combinations
+import collections
+from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
+from functools import lru_cache
+import math
+from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
+import fractions
+from typing import List, Tuple
+import numpy as np
+import random
+import heapq
+from heapq import *
+from dataclasses import dataclass
 
-def C(n,k):
-  return (Fact(n) // ( Fact(k) * Fact(n-k) ) )
-  
-n = int ( input() ) - 1 
-
-print ( C(n+5,n)*C(n+3,3) )
-
-
-
+import builtins
+import re
