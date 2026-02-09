007ef3ab1917bc9b57b8ca1970de4d56a385dac2
Analyzer <analyzer@test.com>
2026-02-09 15:00:07 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,13 @@
-class Solution:
-    def minKBitFlips(self, A: List[int], K: int) -> int:
-        N = len(A)
-        diff = [0] * (N + 1)
-        current_flips = 0
-        flip_count = 0
-        
-        for i in range(N):
-            current_flips += diff[i]
-            actual = A[i] ^ (current_flips % 2)
-            
-            if actual == 0:
-                if i + K > N:
-                    return -1
-                flip_count += 1
-                current_flips += 1
-                if i + K < N:
-           
+from collections import deque
+
+def canVisitAllRooms(rooms):
+    visited = set([0])
+    queue = deque([0])
+    
+    while queue:
+        current_room = queue.popleft()
+        for key in rooms[current_room]:
+            if key not in visited:
+                visited.add(key)
+                queue.append(key)
+    return len(visited) == len(rooms)
