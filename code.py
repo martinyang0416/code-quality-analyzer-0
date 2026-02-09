c922d743b54c5cbdfffb34cf0d85754412cd5604
Analyzer <analyzer@test.com>
2026-02-09 14:30:03 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,18 @@
-import heapq
-
-def maxProbability(n, edges, succProb, start, end):
-    adj = [[] for _ in range(n)]
-    for i in range(len(edges)):
-        a, b = edges[i]
-        p = succProb[i]
-        adj[a].append((b, p))
-        adj[b].append((a, p))
-    
-    max_prob = [0.0] * n
-    max_prob[start] = 1.0
-    heap = [(-1.0, start)]  # Using min-heap to simulate max-heap
-    
-    while heap:
-        current_neg_prob, node = heapq.heappop(heap)
-        current_prob = -current_neg_prob
-        
-        if node
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        MOD = 10**9 + 7
+        transitions = [
+            [4, 6],          # 0
+            [6, 8],          # 1
+            [7, 9],          # 2
+            [4, 8],          # 3
+            [0, 3, 9],       # 4
+            [],               # 5
+            [0, 1, 7],       # 6
+            [2, 6],          # 7
+            [1, 3],          # 8
+            [2, 4]           # 9
+        ]
+        if n == 1:
+            return 10
+        pre
