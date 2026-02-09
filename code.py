4c1a5d19066d5ffa943ab0a96b451ab8121a4fff
Analyzer <analyzer@test.com>
2026-02-09 14:28:09 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,21 @@
-def isRobotBounded(instructions):
-    # Directions: north, east, south, west
-    dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    x, y = 0, 0
-    current_dir = 0  # starts facing north
+def replaceWords(dict, sentence):
+    trie = {}
+    for root_word in dict:
+        node = trie
+        for c in root_word:
+            if c not in node:
+                node[c] = {}
+            node = node[c]
+        node['#'] = True
     
-    for c in instructions:
-        if c == 'G':
-            dx, dy = dirs[current_dir]
-            x += dx
-            y += dy
-        elif c == 'L':
-            current_dir = (current_dir - 1) % 4
-        elif c == 'R':
-            current_dir = (current_dir + 1) % 4
-    
-    # Check if back to origin or direct
+    words = sentence.split()
+    result = []
+    for word in words:
+        current = trie
+        prefix = []
+        found = False
+        for c in word:
+            if c not in current:
+                break
+            current = current[c]
+            prefi
