dadc7c93e1954930881e99def4f7e8dddfd3d6d0
Analyzer <analyzer@test.com>
2026-02-09 15:04:53 -0800

    Update

--- code.py
+++ code.py
@@ -1,5 +1,18 @@
-n = int(input())
-result = 0
-for x in range(1, n):
-    result += (n - 1) // x
-print(result)
+class FenwickTree:
+    def __init__(self, size):
+        self.n = size
+        self.tree = [0] * (self.n + 1)  # 1-based indexing
+
+    def update(self, idx, delta):
+        while idx <= self.n:
+            self.tree[idx] = (self.tree[idx] + delta) % 2
+            idx += idx & -idx
+
+    def query(self, idx):
+        res = 0
+        while idx > 0:
+            res = (res + self.tree[idx]) % 2
+            idx -= idx & -idx
+        return res
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
