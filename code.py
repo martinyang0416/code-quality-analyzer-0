7688a52e8fc40d1792fef8ce204a025804d470a4
Analyzer <analyzer@test.com>
2026-02-09 14:19:11 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,9 @@
-def maxProduct(nums):
+def lengthOfLIS(nums):
     if not nums:
         return 0
-    current_max = current_min = result = nums[0]
-    for num in nums[1:]:
-        temp_max = max(num, current_max * num, current_min * num)
-        temp_min = min(num, current_max * num, current_min * num)
-        current_max, current_min = temp_max, temp_min
-        result = max(result, current_max)
-    return result
+    dp = [1] * len(nums)
+    for i in range(len(nums)):
+        for j in range(i):
+            if nums[i] > nums[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    return max(dp)
