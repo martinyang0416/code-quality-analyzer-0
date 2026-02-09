45521898bd461ff102cb8191b1229556a9297d28
Analyzer <analyzer@test.com>
2026-02-09 14:46:08 -0800

    Update

--- code.py
+++ code.py
@@ -1,9 +1,11 @@
-def findPeakElement(nums):
-    left, right = 0, len(nums) - 1
-    while left < right:
-        mid = (left + right) // 2
-        if nums[mid] < nums[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid
-    return left
+def rankTeams(votes):
+    if not votes:
+        return ""
+    n = len(votes[0])
+    teams = list(votes[0])
+    count = {team: [0] * n for team in teams}
+    for vote in votes:
+        for pos, team in enumerate(vote):
+            count[team][pos] += 1
+    sorted_teams = sorted(teams, key=lambda x: (tuple(-count[x][i] for i in range(n)), x))
+    return ''.join(sorted_teams)
