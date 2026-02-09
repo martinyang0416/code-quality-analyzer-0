a4449fb4b7e298ce0d5bb940c6d90b9b98d93dd8
Analyzer <analyzer@test.com>
2026-02-09 11:26:49 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,18 @@
-import bisect
+def putaway(A: int, B: int, T: int, X: list, Y: list, W: list, S: list) -> int:
+    max_X = -float('inf') if A == 0 else max(X)
+    max_Y = -float('inf') if B == 0 else max(Y)
 
-def putaway(A, B, T, X, Y, W, S):
-    sorted_X = sorted(X)
-    sorted_Y = sorted(Y)
-    
-    W_only = 0
-    S_only = 0
-    Both = 0
-    
-    for i in range(T):
-        w = W[i]
-        s = S[i]
-        # Check if can be handled by weak robots
-        idx_x = bisect.bisect_right(sorted_X, w)
-        weak_ok = idx_x < len(sorted_X)
-        
-        # Check if can be handled by small robots
-        idx_y = bisect.bisect_right(sorted_Y, s)
-        small_ok = idx_y < len(sorted_Y)
-    
+    toys = [(W[0], S[0]), (W[1], S[1])]
+    possible = []
+
+    for w, s in toys:
+        can_weak = (max_X > w) if (A != 0) else False
+        can_small = (max_Y > s) if (B != 0) else False
+
+        allowed = []
+        if can_weak:
+            allowed.append('W')
+        if can_small:
+            allowed.append('S')
+
+   
