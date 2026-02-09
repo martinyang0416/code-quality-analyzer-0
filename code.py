5571cde95d947871594c47aca628d813c11aeb42
Analyzer <analyzer@test.com>
2026-02-09 14:44:04 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,12 @@
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        # Phase 1: Find the intersection point of the two runners.
-        tortoise = hare = nums[0]
-        while True:
-            tortoise = nums[tortoise]
-            hare = nums[nums[hare]]
-            if tortoise == hare:
-                break
+    def isMatch(self, s: str, p: str) -> bool:
+        len_s, len_p = len(s), len(p)
+        dp = [[False] * (len_p + 1) for _ in range(len_s + 1)]
+        dp[len_s][len_p] = True
         
-        # Phase 2: Find the entrance to the cycle.
-        tortoise = nums[0]
-        while tortoise != hare:
-            tortoise = nums[tortoise]
-            hare = nums[
+        for i in range(len_s, -1, -1):
+            for j in range(len_p - 1, -1, -1):
+                first_match = i < len_s and (p[j] == s[i] or p[j] == '.')
+                if j + 1 < len_p and p[j + 1] == '*':
+                    dp[i][j] = dp[i][j + 2] or (first_match and dp[i + 1][j])
+   
