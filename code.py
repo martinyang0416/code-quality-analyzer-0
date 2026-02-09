3816e081442a322f88c6707e4f3964ba9ff4f9f9
Analyzer <analyzer@test.com>
2026-02-09 14:30:38 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,12 @@
-class Solution:
-    def knightDialer(self, n: int) -> int:
-        MOD = 10**9 + 7
-        transitions = [
-            [4, 6],          # 0
-            [6, 8],          # 1
-            [7, 9],          # 2
-            [4, 8],          # 3
-            [0, 3, 9],       # 4
-            [],               # 5
-            [0, 1, 7],       # 6
-            [2, 6],          # 7
-            [1, 3],          # 8
-            [2, 4]           # 9
-        ]
-        if n == 1:
-            return 10
-        pre
+def baseNeg2(N):
+    if N == 0:
+        return "0"
+    res = []
+    while N != 0:
+        remainder = N % (-2)
+        N = N // (-2)
+        if remainder < 0:
+            remainder += 2
+            N += 1
+        res.append(str(remainder))
+    return ''.join(reversed(res))
