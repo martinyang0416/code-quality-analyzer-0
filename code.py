eeca9310f6684bedfbe0b2aee206e3df056620be
Analyzer <analyzer@test.com>
2026-02-09 14:28:46 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,21 @@
-def replaceWords(dict, sentence):
-    trie = {}
-    for root_word in dict:
-        node = trie
-        for c in root_word:
-            if c not in node:
-                node[c] = {}
-            node = node[c]
-        node['#'] = True
+def minTaps(n, ranges):
+    max_reach = [0] * (n + 1)
+    for i in range(n + 1):
+        r = ranges[i]
+        s = max(0, i - r)
+        e = min(n, i + r)
+        if s > e:
+            continue
+        max_reach[s] = max(max_reach[s], e)
     
-    words = sentence.split()
-    result = []
-    for word in words:
-        current = trie
-        prefix = []
-        found = False
-        for c in word:
-            if c not in current:
-                break
-            current = current[c]
-            prefi
+    current_end = 0
+    next_end = 0
+    taps = 0
+    
+    for i in range(n + 1):
+        if i > next_end:
+            return -1
+        next_end = max(next_end, max_reach[i])
+        if i == current_end:
+            if current_end >= n:
+                bre
