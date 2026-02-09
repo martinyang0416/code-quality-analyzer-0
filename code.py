d476dae52200feb06d20f6056c0a1ce0373f14b6
Analyzer <analyzer@test.com>
2026-02-09 11:34:53 -0800

    Update

--- code.py
+++ code.py
@@ -3,23 +3,21 @@ import bisect
 def main():
     import sys
     input = sys.stdin.read().split()
-    idx = 0
-    N = int(input[idx])
-    idx += 1
-    A = list(map(int, input[idx:idx+N]))
-    idx += N
-    sorted_S = sorted(A)
-    prefix = [0] * (N + 1)
-    for i in range(N):
-        prefix[i+1] = prefix[i] + sorted_S[i]
-    
-    T = 0
-    for i in range(N):
-        T += sorted_S[i] * (i + 1)
-    
-    Q = int(input[idx])
-    idx += 1
+    ptr = 0
+    N = int(input[ptr])
+    ptr += 1
+    a = list(map(int, input[ptr:ptr+N]))
+    ptr += N
+    Q = int(input[ptr])
+    ptr += 1
+    queries = []
     for _ in range(Q):
-        i = int(input[idx])
-        j = int(input[idx+1])
- 
+        i = int(input[ptr])
+        j = int(input[ptr+1])
+        queries.append((i, j))
+        ptr += 2
+
+    # Prepare the initial sorted array and prefix sums
+    S = sorted(a, reverse=True)
+    prefix = [0] * (N + 1)
+    for i in range(N
