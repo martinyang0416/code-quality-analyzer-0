5d32d5746d371613bae8c0cf4d75b4272a416208
Analyzer <analyzer@test.com>
2026-02-09 14:38:36 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,5 @@
-def convert(s: str, numRows: int) -> str:
-    if numRows == 1:
-        return s
-    cycle_length = 2 * numRows - 2
-    rows = [''] * numRows
-    for i, char in enumerate(s):
-        pos = i % cycle_length
-        row = pos if pos < numRows else cycle_length - pos
-        rows[row] += char
-    return ''.join(rows)
+def angleClock(hour: int, minutes: int) -> float:
+    hour_angle = (hour % 12) * 30 + 0.5 * minutes
+    minute_angle = 6 * minutes
+    diff = abs(hour_angle - minute_angle)
+    return min(diff, 360 - diff)
