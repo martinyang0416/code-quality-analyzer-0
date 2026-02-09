7430696f7fc96fe3c42ede50bb80feaaea1a7c8e
Analyzer <analyzer@test.com>
2026-02-09 13:31:05 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,18 @@
-n = int(input())
-shop_masks = []
-for _ in range(n):
-    f = list(map(int, input().split()))
-    mask = 0
-    for j in range(10):
-        if f[j]:
-            mask |= 1 << j
-    shop_masks.append(mask)
-p = []
-for _ in range(n):
-    p.append(list(map(int, input().split())))
-max_total = -float('inf')
-for joisino_mask in range(1, 1 << 10):
-    total = 0
-    for i in range(n):
-        overlap = joisino_mask & shop_masks[i]
-        c = bin(overlap).count('1')
-        total += p[i][c]
-    if total > ma
+import math
+
+def compute_intervals(a_seg, b_seg, R):
+    a_t_start, a_t_end, a_vx, a_vy, a_x0, a_y0 = a_seg
+    b_t_start, b_t_end, b_vx, b_vy, b_x0, b_y0 = b_seg
+
+    t_start = max(a_t_start, b_t_start)
+    t_end = min(a_t_end, b_t_end)
+    if t_start >= t_end:
+        return []
+
+    a = a_vx - b_vx
+    c1 = (a_x0 - b_x0) - a_vx * a_t_start + b_vx * b_t_start
+    b_coeff = a_vy - b_vy
+    c2 = (a_y0 - b_y0) - a_vy * a_t_start + b_vy * b_t_start
+
+    A = a**2 + b_coeff**2
+    B = 2 * (a * c1 + b
