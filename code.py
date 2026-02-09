49e0a6bca679358f273b4d760f4fafd8d3cda7f9
Analyzer <analyzer@test.com>
2026-02-09 10:09:17 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,17 @@
-import math
+n = int(input())
+L = list(map(int, input().split()))
+path = [0] + L + [0]
 
-b, m = map(int, input().split())
-e = b + m - 1
+original = 0
+for i in range(1, len(path)):
+    original += abs(path[i] - path[i-1])
 
-k = math.ceil(math.sqrt(b))
-l = math.floor(math.sqrt(e))
-
-if l < k:
-    print(0)
-else:
-    print(l - k + 1)
+for i in range(1, n+1):
+    pre = path[i-1]
+    curr = path[i]
+    next_val = path[i+1]
+    a = abs(curr - pre)
+    b = abs(next_val - curr)
+    c = abs(next_val - pre)
+    new_dist = original - a - b + c
+    print(new_dist)
