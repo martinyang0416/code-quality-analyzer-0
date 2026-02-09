46610a9e4378b20ee3e8043e08832112c181413c
Analyzer <analyzer@test.com>
2026-02-09 13:53:04 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,16 @@
-n, m = map(int, input().split())
-v = list(map(int, input().split()))
-total = 0
-for _ in range(m):
-    x, y = map(int, input().split())
-    total += min(v[x-1], v[y-1])
-print(total)
+m = list(map(int, input().split()))
+w = list(map(int, input().split()))
+hs, hu = map(int, input().split())
+
+total_score = 0
+
+for i in range(5):
+    x = 500 * (i + 1)
+    mi = m[i]
+    wi = w[i]
+    part1 = 0.3 * x
+    part2 = x * (1 - mi / 250) - 50 * wi
+    total_score += max(part1, part2)
+
+total_score += 100 * hs - 50 * hu
+print(int(total_score))
