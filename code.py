6698b706e8100dca34226ae4096740592603fc0a
Analyzer <analyzer@test.com>
2026-02-09 10:43:45 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,19 @@
 import sys
 
-def generate_pairings(elements):
-    if not elements:
+def generate_partitions(remaining):
+    if not remaining:
         return [ [] ]
-    first = elements[0]
-    result = []
-    for i in range(1, len(elements)):
-        pair = (first, elements[i])
-        remaining = elements[1:i] + elements[i+1:]
-        for sub in generate_pairings(remaining):
-            result.append([pair] + sub)
-    return result
+    first = remaining[0]
+    res = []
+    for i in range(1, len(remaining)):
+        second = remaining[i]
+        new_remaining = remaining[1:i] + remaining[i+1:]
+        for p in generate_partitions(new_remaining):
+            res.append( [ (first, second) ] + p )
+    return res
 
-# Precompute all possible pairings of 1-6
-all_pairings = generate_pairings(list(range(1,7)))
+# Precompute all possible partitions for 6 elements
+all_partitions = generate_partitions([1,2,3,4,5,6])
 
 def main():
-    T = int(sys.stdin.readline())
+    inpu
