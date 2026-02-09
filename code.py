8f69dfd1479a744a4bfbd530612533a2b91c866e
Analyzer <analyzer@test.com>
2026-02-09 13:32:34 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,20 @@
-n = int(input())
-digits = list(map(int, input().split()))
-allowed = set(digits)
+import sys
+from collections import defaultdict
 
-all_abc = []
-for a in digits:
-    for b in digits:
-        for c in digits:
-            all_abc.append(a * 100 + b * 10 + c)
+def main():
+    kx, ky = map(int, sys.stdin.readline().split())
+    rook_positions = set()
+    rooks = {}
+    rows = defaultdict(int)
+    cols = defaultdict(int)
+    for i in range(666):
+        x, y = map(int, sys.stdin.readline().split())
+        rooks[i+1] = (x, y)
+        rook_positions.add((x, y))
+        rows[x] += 1
+        cols[y] += 1
 
-all_de = []
-for d in digits:
-    for e in digits:
-        all_de.append(d * 10 + e)
-
-solutions = 0
-
-for abc in all_abc:
-    for de in all_de:
-        d_val = de // 10
-        e_val = de % 10
-        partial1 = abc * e_val
-        if not (100 <= partial1 <= 999):
-            continue
-        i
+    current_king = (kx, ky)
+    for _ in range(2000):
+        kx, ky = current_king
+        best_move = N
