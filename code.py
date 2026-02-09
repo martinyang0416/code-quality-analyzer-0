a2445f590fad1f9a145ebad07031d5c15edf9b47
Analyzer <analyzer@test.com>
2026-02-09 10:15:20 -0800

    Update

--- code.py
+++ code.py
@@ -1,6 +1,19 @@
-n, m = map(int, input().split())
-rows = (1 << n) - 1
-cols = (1 << m) - 1
+import sys
+from collections import defaultdict
 
-for _ in range(rows):
-    print('0' * cols)
+def main():
+    n = int(sys.stdin.readline())
+    vendors = list(map(int, sys.stdin.readline().split()))
+    m = int(sys.stdin.readline())
+    f1 = list(map(int, sys.stdin.readline().split()))
+    f2 = list(map(int, sys.stdin.readline().split()))
+    
+    first_counts = defaultdict(int)
+    second_total = defaultdict(int)
+    
+    for j in range(m):
+        a = f1[j]
+        b = f2[j]
+        first_counts[a] += 1
+        if a != b:
+            seco
